331b6e05b9cb10dd13b3dfde02fb88db6498ba60
adriancs <avoomj@yahoo.com>
2022-09-05 22:48:16 +0800

    migrate dependencies to MySqlConnector (MIT)

--- source code/MySqlBackupAll/Form1.cs
+++ source code/MySqlBackupAll/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using MySql.Data.MySqlClient;
+using MySqlConnector;
 using System.IO;
 
 namespace MySqlBackupAll
